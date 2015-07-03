0531c0c4e3b45d206d997a9197c00ed95840aaa3
digitalglade1 <github.digitalglade@antjoule.com>
2015-07-03 20:25:31 +0000

    Added iteration and release combo boxes

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{ html:'Our first app!'},
+    items:[{ html:'Our first app!'}, {xtype: 'rallyiterationcombobox'}, {xtype: 'rallyreleasecombobox'}], 
+    layout: 'hbox',
     launch: function() {
         //Write app code here
         console.log('our first app v2');
